ecc143dcafa8f03059ad94df7e803da8f2065568
sanguneo <sknah0319@gmail.com>
2025-04-28 16:41:28 +0900

    :tada: 테스트

--- assets/js/example.js
+++ assets/js/example.js
@@ -1,1 +1,8 @@
-console.log('example');
+async function callCreateIssueHello() {
+  await axios.get('https://lxxofjjdpxrprtapiwww.supabase.co/functions/v1/createissue').then(console.log);
+}
+
+window.addEventListener('DOMContentLoaded', async ()=> {
+  // functions
+})
+
